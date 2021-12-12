fc8f4c0275489b8b347f67edea1e9391e5a721c3
joe <hudakjoe@msn.com>
2021-12-11 19:27:14 -0500

    more js adding and working on color change for uvindex

--- assets/weather.js
+++ assets/weather.js
@@ -9,6 +9,7 @@ const cityDate = $('date');
 const cityTemp = $('temp');
 const humidity = $('humidity');
 const windSpeed = $('wind');
+// need to figure out to now get every thing to long and latt for this to work 0_0
 const uvIndex = $('');
 const cityHistory = $('city');
 const cityIcon = $('icon');
@@ -19,31 +20,51 @@ const cityIcon = $('icon');
 var cities = [];
 
 // saving citys information
-
+// alright lost af not even sure how to geather all the citys like What -_- probs a for loop with a if and at the end
 var searchHistory = function () {
+  const cityName = function (citySearch) {
+    for (let i = 0; i < city.length; i++) {}
+  };
+
   cityHistory = JSON.parse(localStorage.getItem('city'));
 };
 
 // api call to get the info i need
-const cityName = function(citySearch)
-
-
 const getWeather = async function () {
-    let response = await fetch(
+  let response = await fetch(
     //   useing charlotte to make sure it works  but need to end up being ${cityName}
     `https://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&appid=${apiKey}`
   );
 
   let data = await response.json();
 
-  console.log(data);
-  const { temp, humidity, icon, speed, dt: cityDate, name } = data.main;
+  const {
+    temp,
+    humidity,
+    icon,
+    speed,
+    dt: cityDate,
+    name,
+    lat,
+    lon,
+  } = data.main;
 
-  postWeather(temp, humidity, icon, speed, cityDate, name);
+  // making a second call to get the long a latt from the the 1st call to the second for uvIndex
+  let secondResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
+  );
+
+  let secondData = await secondResponse.json();
+
+  const { uvi } = secondData;
+
+  console.log(data);
+
+  postWeather(temp, humidity, icon, speed, cityDate, name, uvi);
 };
 
-//  sapossed to be a 5 day forcast
-`https//:api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${Apikey}`
+//  sapossed to be a 5 day forcast need a way to make it fill up each car and prob one of those i++ soo it goes from the current date and adds 5 more WiP
+`https//:api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apikey}`;
 // only sends the search querry when clicked?
 
 searchButton.click(getWeather);
@@ -58,6 +79,26 @@ const postWeather = function (temp, humidity, icon, speed, cityDate, name) {
   humidity.html(humidity);
   cityDate.html(citydate);
   localStorage.setItem('city', JSON.stringify(cityHistory));
+  // how do i make this a box
+  uvIndex.html();
+};
+
+// change color when the uv is too much
+// ummm what is a not ok uv #blessed to be colored
+const changeColors = function () {
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+    if (uv > i) {
+      input.addClass('good');
+    }
+    if (uv === i) {
+      input.addClass('moderate');
+    }
+    if (uv < i) {
+      input.addClass('bad');
+    }
+  }
 };
 
 var searchHistory = function () {
